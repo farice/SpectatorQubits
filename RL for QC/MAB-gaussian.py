8f93867ad24c22b8f23f7bc4b8bd5adad5865a3e
Faris Sbahi <farissbahi@farissbahi-macbookpro.roam.corp.google.com>
2019-12-24 17:22:40 -0500

    refactor to update circuit in-place

--- RL for QC/MAB-gaussian.py
+++ RL for QC/MAB-gaussian.py
@@ -1,6 +1,7 @@
 # %% codecell
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import Aer, BasicAer, execute
+from qiskit.extensions.standard.rz import RZGate
 import matplotlib.pyplot as plt
 import numpy as np
 from qutip import *
@@ -16,7 +17,7 @@ Note, we cannot distinguish between rotations by(pi / 2 + eps, pi / 2 - eps). Th
 
 
 # %% codecell
-def get_spectator_context_circuit(error_theta):
+def create_spectator_context_circuit(error_theta):
     qr = QuantumRegister(1)
     cr = ClassicalRegister(1)
     qc = QuantumCircuit(qr, cr)
@@ -32,6 +33,13 @@ def get_spectator_context_circuit(error_theta):
     return qc
 
 
+# explicit update function allows us to avoid creating a new ciruit object
+# at every iteration
+def update_spectator_circuit(qc, error_theta):
+    inst, qarg, carg = qc.data[1]
+    qc.data[1] = RZGate(error_theta), qarg, carg
+
+
 # %% markdown
 For the reward - determining circuit, we:
 - prepare in X(+1) eigenstate
@@ -41,7 +49,7 @@ Hence, p(+1) = cos ^ 2[theta / 2] allowing us to evaluation error correction rew
 
 
 # %% codecell
-def get_spectator_reward_circuit(error_theta):
+def create_spectator_reward_circuit(error_theta):
     qr = QuantumRegister(1)
     cr = ClassicalRegister(1)
     qc = QuantumCircuit(qr, cr)
@@ -57,12 +65,12 @@ def get_spectator_reward_circuit(error_theta):
 
 
 # %% codecell
-qc = get_spectator_context_circuit(0.1 * np.pi)
+qc = create_spectator_context_circuit(0.1 * np.pi)
 qc.draw()
 
 # %% codecell
-qc_small_pos = get_spectator_context_circuit(0.1 * np.pi)
-qc_small_neg = get_spectator_context_circuit(-0.1 * np.pi)
+qc_small_pos = create_spectator_context_circuit(0.1 * np.pi)
+qc_small_neg = create_spectator_context_circuit(-0.1 * np.pi)
 sim_pos = execute(
     qc_small_pos, backend=BasicAer.get_backend('qasm_simulator'),
     shots=1000)
@@ -83,28 +91,28 @@ class MDPNode:
         self.thetas = np.pi / 2 * np.linspace(-1, 1, num_arms)
         # correspondingly indexed (reward | state, action) set
         # samples from beta(S, F) distribution
-        self.rewards = np.ones(num_arms, dtype=np.float64)
+        self.estimated_rewards = np.ones(num_arms, dtype=np.float64)
         # (successes | arm)
         self.S = np.ones(num_arms, dtype=np.int)
         # (failures | arm)
         self.F = np.ones(num_arms, dtype=np.int)
 
     def resample_rewards(self):
-        # rewards[arm] is drawn from beta(S[arm], F[arm]) distribution
+        # estimated_rewards is drawn from beta(S, F) distribution for each arm
         # which enables exploration
-        for i in range(len(self.rewards)):
-            self.rewards[i] = np.random.beta(self.S[i], self.F[i])
+        for i in range(len(self.estimated_rewards)):
+            self.estimated_rewards[i] = np.random.beta(self.S[i], self.F[i])
 
     def optimal_theta(self):
-        return self.thetas[np.argmax(self.rewards)]
+        return self.thetas[np.argmax(self.estimated_rewards)]
 
     def success(self):
         # update assuming success occurred when pulling currently optimal arm
-        arm = np.argmax(self.rewards)
+        arm = np.argmax(self.estimated_rewards)
         self.S[arm] += 1
 
     def failure(self):
-        arm = np.argmax(self.rewards)
+        arm = np.argmax(self.estimated_rewards)
         self.F[arm] += 1
 
 
@@ -117,12 +125,16 @@ def mab(error_samples, num_arms=11):
 
     process_fidelity_corrected = np.zeros(N)
     process_fidelity_noop = np.zeros(N)
+
+    spectator_context_qc = create_spectator_context_circuit(0)
+    spectator_reward_qc = create_spectator_reward_circuit(0)
     for i in range(N):
-        spectator_qc_1 = get_spectator_context_circuit(error_samples[i])
+        update_spectator_circuit(spectator_context_qc, error_samples[i])
 
         # single measurement of first spectator qubit
         sim_1 = execute(
-            spectator_qc_1, backend=BasicAer.get_backend('qasm_simulator'),
+            spectator_context_qc, backend=BasicAer.get_backend(
+                'qasm_simulator'),
             shots=1)
         outcome_1 = int(
             list(sim_1.result().get_counts().keys())[0]
@@ -135,11 +147,11 @@ def mab(error_samples, num_arms=11):
         correction_theta = context.optimal_theta()
 
         # rotations along the same axis commute
-        spectator_qc_2 = get_spectator_reward_circuit(
-            error_samples[i] + correction_theta)
+        update_spectator_circuit(spectator_reward_qc,
+                                 error_samples[i] + correction_theta)
         sim_2 = execute(
-            spectator_qc_2, backend=BasicAer.get_backend('qasm_simulator'),
-            shots=1)
+            spectator_reward_qc,
+            backend=BasicAer.get_backend('qasm_simulator'), shots=1)
         outcome_2 = int(
             list(sim_2.result().get_counts().keys())[0]
         )
@@ -178,8 +190,8 @@ for alpha in alpha_list:
 
 
 # %% codecell
-print(V0_sequence[0].rewards)
-print(V1_sequence[0].rewards)
+print(V0_sequence[0].estimated_rewards)
+print(V1_sequence[0].estimated_rewards)
 
 
 # %% codecell
