3cbea559617645e14ff9ade48ce80d23acbaabdd
DeepLearning VM <farissbahi@gmail.com>
2020-11-30 09:26:32 +0000

    latest keldysh

--- keldysh_sim.py
+++ keldysh_sim.py
@@ -1,15 +1,13 @@
 import numpy as np
 
 from qutip import *
-from qutip.qip.operations import rx
-from pylab import *
 
-import functools
 import itertools
 
-from numba import njit, jit
+from numba import njit
 
 import keldysh_utils as utils
+
 
 def generate_contour_trajectory_sums(m, T):
     r = []
@@ -25,7 +23,7 @@ def generate_contour_trajectory_sums(m, T):
 def generate_contour_trajectories_for_sums(s, T):
     def integer_division(a, b):
         return -(-a // b) if a < 0 else a // b
-    ones =  T // 2 + integer_division(s, 2)
+    ones = T // 2 + integer_division(s, 2)
     zeros = T // 2 - integer_division(s, 2)
     if np.abs(s) % 2 == 1 and s < 0:
         zeros += 1
@@ -37,60 +35,68 @@ def generate_contour_trajectories_for_sums(s, T):
 
     return np.array(list(set(itertools.permutations(path, len(path)))))
 
-
-@njit # Ridiculous improvement in runtime. Optimizes linalg and loops.
-def keldysh_qprob(observable_spectra, q_prob, H, l_perms, r_perms, state0, T):
-    for l_perm in l_perms:
-        for r_perm in r_perms:
+# Optimizes linalg and loops.
+# @njit
+def keldysh_qprob(observable_spectra, q_prob, q_prob_bdry, H, l_perms, r_perms, state0, T):
+    for l_idx, l_perm in enumerate(l_perms):
+        for r_idx, r_perm in enumerate(r_perms):
             # Delta on L and R path end-points.
             if l_perm[-1] != r_perm[-1]:
                 continue
-            base_prob = (observable_spectra[l_perm[0]]).conjugate().transpose().dot(
+            base_prob = (observable_spectra[0][l_perm[0]]).conjugate().transpose().dot(
                 state0).dot(
                 (state0).conjugate().transpose()).dot(
-                observable_spectra[r_perm[0]])
+                observable_spectra[0][r_perm[0]])
             A_L = np.array([[1+0j]])
             A_R = np.array([[1+0j]])
             for i in range(T - 1):
-                eig_l_ket = observable_spectra[l_perm[i]]
-                eig_l_bra = (observable_spectra[l_perm[i+1]]).conjugate().transpose()
+                eig_l_ket = observable_spectra[i % len(observable_spectra)][l_perm[i]]
+                eig_l_bra = (observable_spectra[(i + 1) % len(observable_spectra)][l_perm[i+1]]).conjugate().transpose()
                 A_L *= eig_l_bra.dot(H[i]).dot(eig_l_ket)
 
-                eig_r_ket = observable_spectra[r_perm[i]]
-                eig_r_bra = (observable_spectra[r_perm[i+1]]).conjugate().transpose()
+                eig_r_ket = observable_spectra[i % len(observable_spectra)][r_perm[i]]
+                eig_r_bra = (observable_spectra[(i + 1) % len(observable_spectra)][r_perm[i+1]]).conjugate().transpose()
                 A_R *= eig_r_bra.dot(H[i]).dot(eig_r_ket)
 
             q = A_L * A_R.conjugate() * base_prob
-            # assert np.imag(q) == 0
             q_prob += q
-    return q_prob
+            # We may group probabilities by the contour bdry given the delta
+            q_prob_bdry[l_perm[-1]] += q
+            # assert np.imag(q) == 0
+    return q_prob, q_prob_bdry
 
 
 def sample_gps(meas_names, meas_eigs, M, mu, sigma, T, state0):
     zs = utils.get_support(T)
     keldysh_vec = {}
-
+    keldysh_bdry_vec = {}
     for _ in range(M):
         # Sample GP.
         gp = utils.gaussian_process([], 0, mu, sigma, T)
         h = np.array([np.array((-1j * utils.hamiltonian(gp, i)).expm()) for i in range(T - 1)])
         for n, e in zip(meas_names, meas_eigs):
             qd_i = []
+            qd_bdry_i = []
             for z in zs:
                 sums = generate_contour_trajectory_sums(z, T)
                 q_prob = np.array([[0+0j]])
+                # Assume +/-1 eigenvalues
+                q_prob_bdry = [np.array([[0+0j]]), np.array([[0+0j]])]
                 for s_L, s_R in sums:
                     if (T % 2 == 1 and (s_L % 2 == 0 or s_R % 2 == 0)) \
                       or (T % 2 == 0 and (s_L % 2 == 1 or s_R % 2 == 1)):
                         continue
                     l_perms = generate_contour_trajectories_for_sums(s_L, T)
                     r_perms = generate_contour_trajectories_for_sums(s_R, T)
-                    q_prob = keldysh_qprob(e, q_prob, h, l_perms, r_perms, state0, T)
+                    q_prob, q_prob_bdry = keldysh_qprob(e, q_prob, q_prob_bdry, h, l_perms, r_perms, state0, T)
                 qd_i.append(q_prob)
+                qd_bdry_i.append(q_prob_bdry)
             if n in keldysh_vec:
                 keldysh_vec[n].append(qd_i)
+                keldysh_bdry_vec[n].append(qd_bdry_i)
             else:
                 keldysh_vec[n] = [qd_i]
-    return keldysh_vec
+                keldysh_bdry_vec[n] = [qd_bdry_i]
+    return keldysh_vec, keldysh_bdry_vec
 
 
--- keldysh_utils.py
+++ keldysh_utils.py
@@ -1,14 +1,11 @@
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 from qutip import *
-from qutip.qip.operations import rx
-from pylab import *
 
-import functools
 import itertools
 
-from numba import njit, jit
 
 # Classical stochastic parameterization of sigmax() rotation.
 def gaussian_process(prev, i, mu, sigma, T):
@@ -18,11 +15,13 @@ def gaussian_process(prev, i, mu, sigma, T):
     prev.append(np.random.normal(mu, sigma) * sigmax())
     return gaussian_process(prev, i, mu, sigma, T)
 
+
 # Cached.
 def hamiltonian(gp, t, *args):
     # t is rounded since we're working on the lattice.
     t = int(t)
     return gp[t]
+
 
 def get_support(T):
     def mul(x):
@@ -30,6 +29,7 @@ def get_support(T):
     zs = list(itertools.product(range(T + 1), [1, -1]))
     zs = list(set(map(mul, zs)))
     return np.sort(zs)
+
 
 def keldysh_vec_to_pd(keldysh_vec, T):
     d = {
@@ -51,23 +51,64 @@ def keldysh_vec_to_pd(keldysh_vec, T):
 def keldysh_plot(meas, keldysh_vec, T):
     plt.style.use('seaborn')
     zs = get_support(T)
-    f, a = plt.subplots(int(len(meas) / 2), 2, sharex=True, sharey=True, figsize=(16, 6 * len(meas) / 2))
-#     f.suptitle('Keldysh quasi-probability distributions for single-qubit fixed-axis GP error')
+    f, a = plt.subplots(int(len(meas) / 3), 3, sharex=True, sharey=True,
+                        figsize=(24, 6 * len(meas) / 3))
+    # f.suptitle('Keldysh quasi-probability distributions for single-qubit fixed-axis GP error')
     for idx, (k, _) in enumerate(meas.items()):
-        ax = a[idx % 2, idx // 2]
+        ax = a[idx // 3, idx % 3]
         corr_vec = keldysh_vec[k]
         for corr in corr_vec:
             ax.plot(zs, corr, alpha=0.3)
 
         ax.plot(zs, np.mean(corr_vec, axis=0), 'r', label='mean')
-        title = fr'${k}-basis$'
+        title = fr'${k}$'
         ax.set_title(title)
         ax.set_ylabel(r'$qp(z)$')
         ax.set_xlabel(r'$z$')
         ax.legend()
     f.show()
-    
-    
+
+
+def keldysh_bdry_plot(meas, keldysh_bdry_vec, T, eig=None):
+    plt.style.use('seaborn')
+    zs = get_support(T)
+    f, a = plt.subplots(int(len(meas) / 3), 3, sharex=True, sharey=True,
+                        figsize=(24, 6 * len(meas) / 3))
+    # f.suptitle('Keldysh quasi-probability distributions for single-qubit fixed-axis GP error')
+    for idx, (k, _) in enumerate(meas.items()):
+        ax = a[idx // 3, idx % 3]
+        corr_vec = keldysh_bdry_vec[k]
+
+        labels = {0 : r"$-1$ at boundary $\tau$", 1 : r"$+1$ at boundary $\tau$"}
+        for corr in corr_vec:
+#             print(corr[:, 0])
+            if eig == 1:
+                ax.plot(zs, corr[:, 1], alpha=0.3, label=labels[1])
+                labels[1] = "_nolegend_"
+            elif eig == -1:
+                ax.plot(zs, corr[:, 0], alpha=0.3, label=labels[0])
+                labels[0] = "_nolegend_"
+            else:
+                ax.plot(zs, corr[:, 1], alpha=0.3, label=labels[1])
+                labels[1] = "_nolegend_"
+                ax.plot(zs, corr[:, 0], alpha=0.3, label=labels[0])
+                labels[0] = "_nolegend_"
+
+        if eig == 1:
+            ax.plot(zs, np.mean(corr_vec[:, :, 1], axis=0), 'r', label='mean (+1)')
+        elif eig == -1:
+            ax.plot(zs, np.mean(corr_vec[:, :, 0], axis=0), 'b', label='mean (-1)')
+        else:
+            ax.plot(zs, np.mean(corr_vec[:, :, 0], axis=0), 'b', label='mean (-1)')
+            ax.plot(zs, np.mean(corr_vec[:, :, 1], axis=0), 'r', label='mean (+1)')
+        title = fr'${k}$'
+        ax.set_title(title)
+        ax.set_ylabel(r'$qp(z)$')
+        ax.set_xlabel(r'$z$')
+        ax.legend()
+    f.show()
+
+
 def qobj_to_numpy(keldysh_vec):
     for k in keldysh_vec:
         cv = keldysh_vec[k]
